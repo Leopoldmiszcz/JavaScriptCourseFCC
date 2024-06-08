55d353270e497a04ce11c9f71a215c5a2fa67c72
Leopold Kaniewski <leopoldmiszcz@outlook.com>
2024-06-08 22:10:35 +0200

    fist commit'

---
+++ Variables/function.js
@@ -0,0 +1,13 @@
+function sayHi(name) {
+    console.log('Hi ' + name);
+}
+
+sayHi('Leo');
+
+// TYPES OF FUNCTIONS
+
+function multiply(num1, num2) {
+    return num1*num2;
+}
+
+console.log(multiply(1, 2));
---
+++ Variables/variable.js
@@ -0,0 +1,41 @@
+let favFruit = "lemons";
+
+let numberOfDonuts = 12;
+let pi = 3.14;
+
+let veryLargeNumber = 3495835734805735n;
+
+let lovesCoding = true;
+
+let favColor;
+console.log(favColor);
+
+favFruit = null;
+
+const uniqeKey = Symbol();
+
+// OBJECTS
+
+let course = {
+    name: "JavaScript Course for Beginners",
+    hours: 3
+};
+
+console.log(course.name);
+
+course.name = "Python Corse for Beginners",
+
+console.log(course.name);
+
+let property = 'hours';
+
+console.log(course[property]);
+
+// ARRAYS
+
+let productColors = [blue, green];
+productColors[0] = red;
+
+
+console.log(productColors[0]);
+console.log(productColors.length);
